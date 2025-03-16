e2aa2c9538d89f2c3f13b05040439e5052811f48
Fabio Benjamin <fabiobenjoliveira@gmail.com>
2025-03-16 18:31:17 -0300

    Add files via upload

---
+++ codigo/dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        nome: "Produtos",
+        descricao: "Sendo uma pequena companhia os produtos ainda são pouco diversos.",
+        tags: "produtos, produtos, products",
+        foto: "img1.png",
+    },
+
+    {
+        nome: "Público alvo",
+        descricao: "Produto voltado para pessoas de idades mais avançadas ou pessoas com porblemas intestinais.",
+        tags: "pessoas, Pesssoas, publico, público, alvo, target, audience",
+        foto: "img2.png",
+    },
+
+    {
+        nome: "Produção",
+        descricao: "A pordução é o fator mais importante para isto.",
+        tags: "produção, producao",
+        foto: "img3.png",
+    },
+
+]
